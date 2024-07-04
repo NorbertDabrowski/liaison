d5af94e50ab61f3e77cfb226566e0931a7673f79
Norbert Dabrowski <norbert.dabrowski@yahoo.com>
2024-07-04 10:20:59 +0100

    onion architecture

--- Infrastructure/LiaisonDBContext.cs
+++ Infrastructure/LiaisonDBContext.cs
@@ -3,16 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure
 {
-
     public class LiaisonDBContext : DbContext
     {
-        //public DbSet<WeatherForecastEntity> WeatherForecast { get; set; }
-        public DbSet<WeatherForecastEntity> WeatherForecasts => Set<WeatherForecastEntity>();
-
-
         public LiaisonDBContext(DbContextOptions<LiaisonDBContext> options)
             : base(options)
         {
         }
+
+        public DbSet<WeatherForecastEntity> WeatherForecasts => Set<WeatherForecastEntity>();
     }
 }
